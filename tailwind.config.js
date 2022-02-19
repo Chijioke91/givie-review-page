cf05951e5c5716edb8edc45a0f94bb67e369d01d
cjaytechguy <cjaytechguy@gmail.com>
2022-02-19 21:47:10 +0100

    initial and final update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        ibm: "'IBM Plex Sans Thai Looped', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
